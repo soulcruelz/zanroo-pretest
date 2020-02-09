2cf72e884012f319306788e5c71e2d8d4adec30f
p_k <p.kungvanrattana@gmail.com>
2020-02-09 19:02:55 +0700

    add root router and import prefix naming

--- src/router.js
+++ src/router.js
@@ -1,2 +1,26 @@
-import { BrowserRouter, Route, Link } from "react-router-dom"
+import React from "react"
+import ReactDOM from "react-dom"
+import {
+    HashRouter,
+    Route,
+    Link,
+    Switch,
+} from "react-router-dom"
 
+
+function App() {
+    return (
+        <Switch>
+            <Route exact="/">
+                
+            </Route>
+        </Switch>
+    )
+}
+
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById("root")
+)
